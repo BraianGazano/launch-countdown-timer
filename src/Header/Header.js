174652b3761f1f17690685b121bf9b7a445f7e56
Braian <44708422+BraianGazano@users.noreply.github.com>
2021-11-22 13:47:10 -0300

    Titulo terminado

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import { TitleStyled}  from "./styles/TitleStyled";
+
+const Header = () => {
+    return ( 
+        <>
+        <TitleStyled>
+        We're launching soon
+        </TitleStyled>
+        </>
+     );
+}
+export default Header;
+
+
