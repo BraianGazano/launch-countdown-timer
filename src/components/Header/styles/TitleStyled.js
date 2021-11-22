f61350f2e60515b03206c42dcdd12f4058e8d454
Braian <44708422+BraianGazano@users.noreply.github.com>
2021-11-22 13:48:08 -0300

    Titulo terminado

--- src/components/Header/styles/TitleStyled.js
+++ src/components/Header/styles/TitleStyled.js
@@ -4,7 +4,8 @@ export const TitleStyled = styled.h1`
     display:flex;
     justify-content:center;
     font-size: 2em;
-    color: red;
+    color: hsl(0, 0%, 100%);
+    padding-top: 4em;
     letter-spacing: 0.2em;
     text-transform: uppercase;
 `;
