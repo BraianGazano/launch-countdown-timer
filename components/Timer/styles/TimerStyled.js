0c1d941e0d6af12527936a0a66469a2314d8cb5a
Braian <44708422+BraianGazano@users.noreply.github.com>
2021-11-25 18:23:22 -0300

    Add files via upload

--- components/Timer/styles/TimerStyled.js
+++ components/Timer/styles/TimerStyled.js
@@ -4,21 +4,22 @@ export const TimerStyled = styled.div`
     display:flex;
     justify-content:center;
     padding: 5em;
+    align-items: center;
     & p{
         border-radius: 20px;
-        font-size: 6rem;
+        box-shadow: 0 0.5rem 1px hsl(234, 17%, 12%);
+        font-size: 5rem;
         background: linear-gradient(180deg, hsl(236, 21%, 22%) 50%, hsl(236, 21%, 26%) 50%);
         padding: 0.6em;
-        margin:0.5rem;
+        margin:1.5rem;
         color: hsl(345, 95%, 68%);
     }
     & span{
         color: hsl(237, 18%, 59%);
         text-transform: uppercase;
         letter-spacing: 0.5em;
-        font-size: 1.5em;
+        font-size: 1.3em;
         display:flex;
         justify-content:space-around;
-        text-align:center;
     } 
 `;    
